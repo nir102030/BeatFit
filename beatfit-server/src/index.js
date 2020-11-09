4c7f8b613500864a5eea03a5301708406f38ecee
nir102030 <nir102030@gmail.com>
2020-11-09 12:38:28 +0200

    server-first-commit

---
+++ beatfit-server/src/index.js
@@ -0,0 +1,42 @@
+require('../src/models/User'); // Because we don't want to use it as a variable, we don't use const (we want to use it once)
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const requireAuth = require('../src/middlewares/requireAuth');
+const authRoutes = require('../src/routs/authRouts');
+
+//Create an express server
+const app = express();
+
+//Use the body json parser middleware to allow reading the json files from db
+app.use(bodyParser.json());
+
+//Use the different routes
+app.use(authRoutes);
+
+//Connect to mongo DB
+const mongoUri = 'mongodb+srv://dbNir:bhRvnkl102030@cluster0-xuwhd.mongodb.net/beatfit?retryWrites=true&w=majority';
+mongoose.connect(mongoUri, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+});
+
+//Log success message if the db connection succeed
+mongoose.connection.on('connected', () => {
+	console.log('Connected to mongo instance');
+});
+
+//Log failure message if the db connection failed
+mongoose.connection.on('error', (err) => {
+	console.error('Error connecting to mongo', err);
+});
+
+//Make a get request to the user information (log in),
+//that uses the requireAuth middleware to make sure the user will have access to db only if he have a valid token (jsw)
+app.get('/', requireAuth, (req, res) => {
+	res.send(`Your email: ${req.user.email}`);
+});
+
+app.listen(3000, () => {
+  console.log(`Example app listening at http://localhost:${3000}`)
+})
---
+++ beatfit-server/src/models/User.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+//This model creates a Users collection inside the mongoDB using the mongoose library 
+
+//Define the User object schema inside the db
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+//This is called pre-save hook
+//In other words, this is a function thats going to run before saving an instance of a user to the db
+//The reason we use a function key word instead of arrow function is that we want to use the key word "this" to get the user that been saved
+//If we will use a regular arrow function, the context of "this" will become the entire model
+userSchema.pre('save', function(next) {
+    const user = this;
+
+    //If the password hasn't been changes, we don't want to do nothing (continue to the next middleware)
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    //using the bcrypt library to genrate an "salt" string to be added to the hashed password
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err){
+            return next(err);
+        }
+
+        //hashing the user password and assign it to user.password
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err){
+                return next(err);
+            }
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+//Attaching a new function to the user schema methods
+//that compare the inserted password and the user passward, and returns true if they are identical
+userSchema.methods.comparePassword = function(candidatePassword) {
+    const user = this;
+    //A promise is a way to use async method and return a value. 
+    //Instead of immadiately retuning the final value, the method returns a "promise" to supply the value at some point in the future
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+            if (err){
+                return reject(err);
+            }
+            if (!isMatch) {
+                return reject(false);
+            }
+            resolve(true);
+        });
+    });
+}
+
+mongoose.model('User', userSchema);
+//We are not epxort this model because we want to use it only once - 
+//to establish the User collection in the DB
+//We don't want to establish it twice
